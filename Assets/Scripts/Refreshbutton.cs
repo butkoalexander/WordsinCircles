87a41247d81094e9e023c324fbf60d704500c730
Butko Alexander <53627149+butkoalexander@users.noreply.github.com>
2019-08-06 23:24:51 +0300

    donewithoptimizedfile

--- Assets/Scripts/Refreshbutton.cs
+++ Assets/Scripts/Refreshbutton.cs
@@ -5,10 +5,12 @@ using UnityEngine.UI;
 
 public class Refreshbutton : MonoBehaviour
 {
+    public GameObject textToRefresh;
     public void Clickedrefresh()
     {
         print("Onclick");
-        
+        if(textToRefresh)
+        textToRefresh.GetComponent<Text>().text = "";
     }
 }
 
---
+++ OptimizeDictionary/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OptimizeDictionary
+{
+
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string[] createText=null;
+            string path = @"C:\Users\Leikar\Documents\My Projects\WordsinCircles\OptimizeDictionary\word_rus.txt";
+            string newpath = @"C:\Users\Leikar\Documents\My Projects\WordsinCircles\OptimizeDictionary\newword_rus.txt";
+            string temp;
+            // Open the file to read from.
+            string[] readText = File.ReadAllLines(path);
+            List<string> newText=new List<string>();
+            foreach (string s in readText)
+            {
+                temp = "";
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (!temp.Contains(s[i]))
+                    {
+                        temp += s[i];
+                    }
+                    
+                }
+                if(temp.Length<=5)
+                {
+                    newText.Add(s);
+                }
+
+                   
+            }
+
+
+
+
+          
+                // Create a file to write to.
+               
+                File.WriteAllLines(newpath, newText);
+           
+
+        }
+       
+    }
+}
